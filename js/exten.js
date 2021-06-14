f5168043a2d01d9aa8b0bdeb01982d571b897592
albert-notes <alb105322129@gmail.com>
2021-06-14 23:57:22 +0800

    Site updated: 2021-06-14 23:57:21

--- js/exten.js
+++ js/exten.js
@@ -1,16 +1,3 @@
-/*添加图片top*/
-var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
-
-document.getElementById("go-up").innerHTML += top_up;
-
-var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
-var menus_item_child = mobile_sidebar_menus.getElementsByClassName("menus_item_child");
-var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
-for (var i = 0; i < menus_item_child.length; i++) {
-    menus_item_child[i].style.display = "none";
-    menus_expand[i].className += " closed";
-}
-
 var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
